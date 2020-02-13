77d76897c075c82d3d096a7c83cb4e0003d8f868
Muhammad Rifqi <walarabiah@gmail.com>
2020-02-13 14:38:53 +0700

    12 Februari

--- perulangan_for.php
+++ perulangan_for.php
@@ -14,4 +14,32 @@ for ($i=1; $i<=10; $i+=2){
 }
 
 echo "<hr>";
+
+// contoh 1
+for ($i=1; $i<=10; $i++)
+    echo "$i";
+
+echo "<br><br>";
+
+// contoh 2
+// for ($i=1; ; $i++)
+//     echo "$i > 10";
+
+// echo "<br><br>";
+
+// contoh 3
+// $i = 1;
+// for (; ; )
+//     if($i > 10)
+// break;
+//     echo "$i";
+//     $i++;
+
+// echo "<br><br>";
+
+// contoh 4
+for ($i=1; $i<=10; print "$i", $i++)
+    echo "$i";
+
+echo "<br><br>";
 ?>
---
+++ perulangan_while.php
@@ -0,0 +1,24 @@
+<?php
+echo "<hr>";
+$i=0;
+while($i<=10){
+    echo "$i";
+    $i++;
+}
+
+echo "<hr>";
+
+$i=2;
+while($i<=10){
+    echo "$i<br>";
+    $i+=2;
+}
+$i=1;
+while($i<=10){
+    echo "$i<br>";
+    $i+=2;
+}
+
+echo "<hr>";
+
+?>
