32db47ae3be1e5d7dbd1961f260da3f31c56d077
Muhammad Rifqi <walarabiah@gmail.com>
2020-02-13 15:36:05 +0700

    update

--- perulangan_dowhile.php
+++ perulangan_dowhile.php
@@ -17,5 +17,14 @@ $i=1;
 do{
     echo "$i<br>";
     $i+=2;
-} while ($i<=10)
+} while ($i<=10);
+
+echo "<hr>";
+
+$i=0;
+do{
+    echo "<center><img src=html/icon/Orang.png width=50px><br><br></center>";
+    $i++;
+} while ($i<=10);
+
 ?>
---
+++ perulangan_foreach.php
@@ -0,0 +1,6 @@
+<?php
+$hewan = array('Sapi','Ayam','Kambing','Tokek' => 0,1,2,3 );
+foreach ($hewan as $key => $value) {
+    echo "$hewan[3] <br>";
+}
+?>
